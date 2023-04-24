c5c3209f474155e38b798e7ef5edf8b062dab7a1
1xtend <vlad.mezhevyi@gmail.com>
2023-04-24 14:51:07 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+/*===============Body and Html===============*/
+export const mainElems = {
+  html: document.documentElement,
+  body: document.body,
+};
+
+/*===============isWebp function===============*/
+import { isWebp } from './isWebp.js';
+isWebp();
+
+/*===============Burger===============*/
+import { burger } from './burger.js';
+burger();
+
+/*===============Change Theme===============*/
+// import { changeTheme } from './changeTheme.js';
+// changeTheme();
+
+/*===============Modal function===============*/
+// import { modal } from './modal.js';
+// modal();
+
+/*===============Dropdown===============*/
+import { dropdown } from './dropdown.js';
+dropdown();
+
+/*===============Swiper===============*/
+// import { swiper } from './swiper/swiper.js';
---
+++ src/js/dropdown.js
@@ -0,0 +1,72 @@
+// Import body and html
+import { mainElems } from './app.js';
+
+// Function that checks if the device has a touchscreen
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+export const dropdown = () => {
+  if (isMobile.any() || window.innerWidth <= 768) {
+    mainElems.body.classList.add('touch');
+
+    touchDropdown();
+  } else {
+    mainElems.body.classList.add('mouse');
+  }
+
+  function touchDropdown() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    if (dropdowns.length > 0) {
+      dropdowns.forEach((dropdown) => {
+        const dropdownArrow = dropdown.querySelector('.dropdown-arrow');
+
+        dropdownArrow.addEventListener('click', (e) => {
+          const showDropdown = document.querySelector('.show-dropdown');
+
+          toggleDropdown(dropdown);
+
+          if (showDropdown && showDropdown !== dropdown) {
+            toggleDropdown(showDropdown);
+          }
+        });
+      });
+    }
+
+    function toggleDropdown(dropdown) {
+      const dropdownList = dropdown.querySelector('.dropdown__list');
+
+      if (dropdown.classList.contains('show-dropdown')) {
+        dropdownList.removeAttribute('style');
+        dropdown.classList.remove('show-dropdown');
+      } else {
+        dropdownList.style.height = dropdownList.scrollHeight + 'px';
+        dropdown.classList.add('show-dropdown');
+      }
+    }
+  }
+};
